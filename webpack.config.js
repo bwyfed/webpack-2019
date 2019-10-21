bef43500a540f1bf8d05318dd818c355977ff9d0
Bianwy <bianwy025@qq.com>
2019-10-21 16:22:40 +0800

    初始化项目代码。

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+    sub: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: 'http://cdn.com.cn'
+  },
+  module: {
+    rules: [{
+      test: /\.(jpg|png|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          name: '[name]_[hash].[ext]',
+          outputPath: 'images/',
+          limit: 10240
+        }
+      }
+    }, {
+      test: /\.(eot|ttf|woff|svg)$/,
+      use: {
+        loader: 'file-loader'
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2,
+            modules: false
+          }
+        },
+        'sass-loader',
+        'postcss-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
+}
