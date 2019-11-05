b8d8b9b54428818f136a91c299e289249b1d15cd
Bianwy <bianwy025@qq.com>
2019-11-05 12:59:56 +0800

    调整目录结构。

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // ES Module 模块引入方式
-import Header from './header.js'
-import Sidebar from './sidebar.js'
-import Content from './content.js'
+// import Header from './header.js'
+// import Sidebar from './sidebar.js'
+// import Content from './content.js'
 
-var dom = document.getElementById('root')
+// CommonJS 模块引入方式
+var Header = require('./header.js')
+var Sidebar = require('./sidebar.js')
+var Content = require('./content.js')
 
 new Header()
 new Sidebar()
