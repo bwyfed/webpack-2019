5c8198920dfcff87c47f5bd2509f678e6ec7f48b
Bianwy <bianwy025@qq.com>
2019-11-05 12:38:32 +0800

    初始化项目，引入简单的示例。

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import Header from './header.js'
 import Sidebar from './sidebar.js'
 import Content from './content.js'
 
+var dom = document.getElementById('root')
+
 new Header()
 new Sidebar()
 new Content()
