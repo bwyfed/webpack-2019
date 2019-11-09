0a85e7b6766cd76b90b0ad4b6505b5c6b54b139a
bwyfed <36172659@qq.com>
2019-11-09 19:51:47 +0800

    修正代码。

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,15 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: {
     main: './src/index.js'
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    // publicPath: 'http://cdn.com.cn'
-  },
   module: {
     rules: [{
       test: /\.(jpg|png|gif)$/,
       use: {
-        loader: 'url-loader',
+        loader: 'url-loader', // 或者使用 file-loader
         options: {
           // placeholder 占位符
           name: '[name]_[hash].[ext]',
@@ -24,31 +17,10 @@ module.exports = {
           limit: 10240
         }
       }
-    }, {
-      test: /\.(eot|ttf|woff|svg)$/,
-      use: {
-        loader: 'file-loader'
-      }
-    }, {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            importLoaders: 2,
-            modules: false
-          }
-        },
-        'sass-loader',
-        'postcss-loader'
-      ]
     }]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    }),
-    new CleanWebpackPlugin()
-  ]
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
 }
