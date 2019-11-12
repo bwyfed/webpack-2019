e79fc6555f1cb106165ff7528500325b1e9b78a1
Bianwy <bianwy025@qq.com>
2019-11-12 16:47:08 +0800

    改变webpack相关配置。

--- webpack.config.js
+++ webpack.config.js
@@ -18,16 +18,18 @@ module.exports = {
       use: {
         loader: 'url-loader',
         options: {
+          // placeholder 占位符
           name: '[name]_[hash].[ext]',
           outputPath: 'images/',
           limit: 10240
         }
       }
     }, {
-      test: /\.(eot|ttf|woff|svg)$/,
-      use: {
-        loader: 'file-loader'
-      }
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
     }, {
       test: /\.scss$/,
       use: [
@@ -35,19 +37,27 @@ module.exports = {
         {
           loader: 'css-loader',
           options: {
-            importLoaders: 2,
-            modules: false
+            importLoaders: 2, // 通过@import引入的scss文件，也走postcss-loader和sass-loader处理
+            // 如果是引入字体文件，则是全局引入，不需要配置CSS module
+            // modules: true // 增加CSS module的功能
           }
         },
-        'sass-loader',
-        'postcss-loader'
+        'postcss-loader',
+        'sass-loader'
       ]
+    }, {
+      test: /\.(eot|ttf|woff2?|svg)$/,
+      use: {
+        loader: 'file-loader'
+      }
     }]
   },
   plugins: [
+    // 打包之后运行
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: './src/index.html'
     }),
+    // 打包之前运行
     new CleanWebpackPlugin()
   ]
 }
