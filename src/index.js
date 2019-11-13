40b18d82f17031d6677a6efa2fa1244254b7f12a
Bianwy <bianwy025@qq.com>
2019-11-13 10:29:16 +0800

    '实现基本的babel配置，支持普通业务开发。'

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-// 示例1: CSS文件热重载
-// import './style.css'
-// var btn = document.createElement('button')
-// btn.innerHTML = '新增'
-// document.body.appendChild(btn)
+// import '@babel/polyfill'
 
-// btn.onclick = function() {
-//   var div = document.createElement('div')
-//   div.innerHTML = 'item'
-//   document.body.appendChild(div)
-// }
+const arr = [
+  new Promise(() => {}),
+  new Promise(() => {})
+]
 
-// 示例2：JS代码热重载
-import counter from './counter'
-import number from './number'
-
-counter()
-number()
-
-if(module.hot) {
-  module.hot.accept('./number', () => {
-    document.body.removeChild(document.getElementById('number'))
-    number()
-  })
-}
+arr.map(item => {
+  console.log(item)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,26 @@ module.exports = {
   },
   module: {
     rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                edge: "17",
+                firefox: "60",
+                chrome: "67",
+                safari: "11.1",
+              },
+              useBuiltIns: 'usage'
+            }
+          ]
+        ]
+      }
+    }, {
       test: /\.(jpg|png|gif)$/,
       use: {
         loader: 'url-loader',
