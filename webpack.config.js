2dca17db98839308cc39e1bc582f14944739de3e
Bianwy <bianwy025@qq.com>
2019-11-13 11:06:39 +0800

    '使用runtime插件进行类库打包的示例。'

--- webpack.config.js
+++ webpack.config.js
@@ -29,20 +29,32 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       options: {
-        presets: [
+        plugins: [
           [
-            "@babel/preset-env",
+            "@babel/plugin-transform-runtime",
             {
-              targets: {
-                edge: "17",
-                firefox: "60",
-                chrome: "67",
-                safari: "11.1",
-              },
-              useBuiltIns: 'usage'
+              "absoluteRuntime": false,
+              "corejs": 3, // false->3
+              "helpers": true,
+              "regenerator": true,
+              "useESModules": false
             }
           ]
         ]
+        // presets: [
+        //   [
+        //     "@babel/preset-env",
+        //     {
+        //       targets: {
+        //         edge: "17",
+        //         firefox: "60",
+        //         chrome: "67",
+        //         safari: "11.1",
+        //       },
+        //       useBuiltIns: 'usage'
+        //     }
+        //   ]
+        // ]
       }
     }, {
       test: /\.(jpg|png|gif)$/,
