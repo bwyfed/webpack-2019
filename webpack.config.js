3df22a815e183da50b802e16687462ef044e619a
Bianwy <bianwy025@qq.com>
2019-11-13 12:33:35 +0800

    '使用core-js代替babel-polyfill。'

--- webpack.config.js
+++ webpack.config.js
@@ -33,13 +33,14 @@ module.exports = {
           [
             "@babel/preset-env",
             {
+              useBuiltIns: 'usage',
+              corejs: 3,
               targets: {
                 edge: "17",
                 firefox: "60",
                 chrome: "67",
                 safari: "11.1",
-              },
-              useBuiltIns: 'usage'
+              }
             }
           ]
         ]
